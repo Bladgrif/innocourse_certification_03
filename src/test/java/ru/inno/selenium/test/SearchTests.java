4c5994a44c42f9576953d7f523a0f5e0280d47e3
Badgrif <aswd@asd.com>
2024-06-16 23:49:01 +0300

    I'm hungry

---
+++ src/test/java/ru/inno/selenium/test/SearchTests.java
@@ -0,0 +1,26 @@
+package ru.inno.selenium.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = "edge";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
